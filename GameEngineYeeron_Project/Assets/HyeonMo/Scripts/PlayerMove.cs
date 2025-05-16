8f88e89e8e5d255fb9ee2c35ca9f8f02deba8ae8
HyeonMoKim0 <mo8150h@gmail.com>
2025-05-16 11:20:56 +0900

    플레이어 대시 기능 수정
    - 플레이어 대시 기능을 사용 후 딜레이를 가지도록 수정
    -> 플레이어가 대시를 사용하고, 1초 뒤에 사용할 수 있도록 구현
    * 플레이어 대시는 현재 position을 이용해 순간이동하는 판정으로 만들었으나, 추후 빠르게 움직여서 순간이동하는 것처럼 보이도록 수정할 예정

--- GameEngineYeeron_Project/Assets/HyeonMo/Scripts/PlayerMove.cs
+++ GameEngineYeeron_Project/Assets/HyeonMo/Scripts/PlayerMove.cs
@@ -4,39 +4,45 @@ using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
-    float x, z;
+    float x = 0f, z = 0f;
+    float dashDelay = 0f;
     bool isInputDash;
-    bool isMoving;
     Vector3 playerMoveValue;
     Vector3 playerDashValue;
     Vector3 playerVelocity;
 
     [SerializeField]
     GameObject player;
-    Rigidbody playerRigidbody;
     float moveSpeed = 8.0f;
     float dashSpeed = 8.0f;
 
     void Start() {
-        playerRigidbody = player.GetComponent<Rigidbody>();
+        
+    }
+
+    void Update() {
+        dashDelay += Time.deltaTime;
+        Debug.Log(dashDelay);
+
+        if (dashDelay >= 1 && isInputDash)
+        {
+            dashDelay = 0f;
+            Dash(ref x, ref z);
+        }
     }
 
     void FixedUpdate() {
         x = Input.GetAxisRaw("Horizontal");
         z = Input.GetAxisRaw("Vertical");
-        isInputDash = Input.GetButtonDown("Fire3");
+        isInputDash = Input.GetButton("Fire3"); //GetButton이 제일 인식이 잘되어 선택
+
 
         if (IsMoving(ref x, ref z))
             PlayerMoving(ref x, ref z);
-
-        if (isInputDash)
-            Dash(ref x, ref z);
-            
     }
 
     void Dash(ref float x, ref float z) {
         player.transform.position += playerDashValue = new Vector3(x * dashSpeed, 0, z * dashSpeed);
-        //이새끼 왜 대각선으로만가는데?
 
         Debug.Log("Dash 잘되구여");
     }
