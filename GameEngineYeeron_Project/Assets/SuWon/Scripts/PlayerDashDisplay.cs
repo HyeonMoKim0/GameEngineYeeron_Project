cf98acfd4840a84ec4fcbfe4b9fba1dc93508260
suwon63 <dlatn3482@naver.com>
2025-06-05 19:34:03 +0900

    UI 기능 구현
    실시간 HP DASH UI 구현
    Reward 보상UI창 구현

---
+++ GameEngineYeeron_Project/Assets/SuWon/Scripts/PlayerDashDisplay.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using TMPro;  // TextMeshPro 네임스페이스
+
+public class PlayerDashDisplay: MonoBehaviour
+{
+    public PlayerDashInfermation playerDashInfo;  // PlayerDashInfermation 스크립트 연결
+    public TextMeshProUGUI dashCountText;         // TMP UI 텍스트 연결
+
+    void Update()
+    {
+        // 대시 횟수 텍스트 업데이트
+        if (playerDashInfo != null && dashCountText != null)
+        {
+            dashCountText.text = $"Dashes: {playerDashInfo.dashCurrentStack}";
+        }
+    }
+}
