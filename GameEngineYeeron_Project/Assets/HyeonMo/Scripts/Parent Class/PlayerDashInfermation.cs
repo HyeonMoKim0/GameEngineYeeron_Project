a8febd95f0bf210a057998140be8ca7714a943ad
HyeonMoKim0 <mo8150h@gmail.com>
2025-06-05 13:17:14 +0900

    PlayerDashInfermation 변경
    해당 스크립트 내에 접근 지정자 protected를 모두 public으로 임시 변경

--- GameEngineYeeron_Project/Assets/HyeonMo/Scripts/Parent Class/PlayerDashInfermation.cs
+++ GameEngineYeeron_Project/Assets/HyeonMo/Scripts/Parent Class/PlayerDashInfermation.cs
@@ -2,19 +2,19 @@ using UnityEngine;
 
 public class PlayerDashInfermation : MonoBehaviour {
     [Header("플레이어 대시")]
-
-    [SerializeField] protected int dashCurrentStack = 2;
-    protected int dashMinStack = 0;
-    [SerializeField] protected static int dashMaxStack = 2;
-
-
-    [SerializeField] protected float dashSpeed = 32.0f;
+    //추후 아래 사항들은 protected으로 변경하고, 싱글톤 패턴으로 Instance 제작할 것
+    [SerializeField] public int dashCurrentStack = 2;
+    public int dashMinStack = 0;
+    [SerializeField] public static int dashMaxStack = 2;
 
 
-    protected float dashCurrentDelay = 1.0f;
-    [SerializeField] protected float dashAfterDelay = 1.0f;
+    [SerializeField] public float dashSpeed = 32.0f;
 
 
-    protected float redashCurrentTime = 0f;
-    [SerializeField] protected static float redashConstraintTime = 5.0f;
+    public float dashCurrentDelay = 1.0f;
+    [SerializeField] public float dashAfterDelay = 1.0f;
+
+
+    public float redashCurrentTime = 0f;
+    [SerializeField] public static float redashConstraintTime = 5.0f;
 }
